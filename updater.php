c732e2d7490cc7501085399078680de7eecc857c
prabode <prabode@gmail.com>
2012-02-14 14:02:26 -0500

    updated to version 1.3.8

--- updater.php
+++ updater.php
@@ -28,7 +28,7 @@
 /*
 Plugin Name: GitHub Updater
 Description: Update plugin using git hub.
-Version: 1.3.7
+Version: 1.3.8
 Original Author Joachim Kudish, http://jkudish.com
 Author: Venturit Inc - Narada Jayasingha
 License: GPLv2 or later
