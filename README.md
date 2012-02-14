bf738ff9ff1dc6aabbbf6b311f4063a505350417
prabode <prabode@gmail.com>
2012-02-14 09:11:38 -0500

    updated to 1.2.3

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 This plugin test updating plugin through github 
-`~Current Version:1.2.2~`
+`~Current Version:1.2.3~`
--- updater.php
+++ updater.php
@@ -28,7 +28,7 @@
 /*
 Plugin Name: GitHub Updater
 Description: Custom post type for image galleries.
-Version: 1.2.2
+Version: 1.2.3
 Original Author Joachim Kudish, http://jkudish.com
 Author: Venturit Inc - Narada Jayasingha
 License: GPLv2 or later
