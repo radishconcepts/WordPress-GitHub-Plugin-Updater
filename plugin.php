b3c4851a97187244d54d549024b2691f866f9f21
Matt <info@mgates.me>
2012-06-03 15:36:04 -0400

    Demo was loading the wrong readme

--- plugin.php
+++ plugin.php
@@ -55,7 +55,7 @@ function github_plugin_updater_test_init() {
 			'sslverify' => true,
 			'requires' => '3.0',
 			'tested' => '3.3',
-			'readme' => 'readme.txt'
+			'readme' => 'README.md'
 		);
 
 		new WPGitHubUpdater($config);
