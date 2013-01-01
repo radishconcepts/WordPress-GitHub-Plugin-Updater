88f61a311ec4b1a1b970dd83f43c53d2f37c5899
Joey Kudish <joachim.kudish@gmail.com>
2012-12-31 18:10:15 -0800

    simplify if/return in updater class
    ref #27

--- updater.php
+++ updater.php
@@ -1,39 +1,37 @@
 <?php
 
-// Prevent loading this file directly - Busted!
-if ( !defined( 'ABSPATH' ) )
-	die( '-1' );
+// Prevent loading this file directly and/or if the class is already defined
+if ( ! defined( 'ABSPATH' ) || class_exists( 'WPGitHubUpdater' ) || class_exists( 'WP_GitHub_Updater' ) )
+	return;
 
-if ( ! class_exists( 'WP_GitHub_Updater' ) ) :
-
-	/**
-	 *
-	 *
-	 * @version 1.4
-	 * @author Joachim Kudish <info@jkudish.com>
-	 * @link http://jkudish.com
-	 * @package GithubUpdater
-	 * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
-	 * @copyright Copyright (c) 2011-2013, Joachim Kudish
-	 *
-	 * GNU General Public License, Free Software Foundation
-	 * <http://creativecommons.org/licenses/GPL/2.0/>
-	 *
-	 * This program is free software; you can redistribute it and/or modify
-	 * it under the terms of the GNU General Public License as published by
-	 * the Free Software Foundation; either version 2 of the License, or
-	 * (at your option) any later version.
-	 *
-	 * This program is distributed in the hope that it will be useful,
-	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	 * GNU General Public License for more details.
-	 *
-	 * You should have received a copy of the GNU General Public License
-	 * along with this program; if not, write to the Free Software
-	 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-	 */
-	class WP_GitHub_Updater {
+/**
+ *
+ *
+ * @version 1.4
+ * @author Joachim Kudish <info@jkudish.com>
+ * @link http://jkudish.com
+ * @package WP_GitHub_Updater
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @copyright Copyright (c) 2011-2013, Joachim Kudish
+ *
+ * GNU General Public License, Free Software Foundation
+ * <http://creativecommons.org/licenses/GPL/2.0/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+class WP_GitHub_Updater {
 
 	/**
 	 * Temporary store the data fetched from GitHub, so it only gets loaded once per class instance
@@ -380,7 +378,4 @@ if ( ! class_exists( 'WP_GitHub_Updater' ) ) :
 		return $result;
 
 	}
-
 }
-
-endif; // endif class exists
