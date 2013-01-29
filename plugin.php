b9ae4af5359aadac11d38bc3bb28a8422f894110
raz ohad <admin@bainternet.info>
2013-01-29 18:24:36 +0200

    slimmed down $config array

--- plugin.php
+++ plugin.php
@@ -47,10 +47,7 @@ function github_plugin_updater_test_init() {
 		$config = array(
 			'slug' => plugin_basename( __FILE__ ),
 			'proper_folder_name' => 'github-updater',
-			'api_url' => 'https://api.github.com/repos/jkudish/WordPress-GitHub-Plugin-Updater',
-			'raw_url' => 'https://raw.github.com/jkudish/WordPress-GitHub-Plugin-Updater/master',
 			'github_url' => 'https://github.com/jkudish/WordPress-GitHub-Plugin-Updater',
-			'zip_url' => 'https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/zipball/master',
 			'sslverify' => true,
 			'requires' => '3.0',
 			'tested' => '3.3',
