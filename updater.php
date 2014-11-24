7dc872c4e2b9c66da5b7a196a86f36a09e0de27c
Coen Jacobs <coenjacobs@gmail.com>
2014-11-24 15:12:16 +0100

    Smarter regex

--- updater.php
+++ updater.php
@@ -223,7 +223,7 @@ class WP_GitHub_Updater {
 
 			if (is_array($raw_response)) {
 				if (!empty($raw_response['body']))
-					preg_match( '#^\s*Version\:\s*(.*)$#im', $raw_response['body'], $matches );
+					preg_match( '/.*Version\:\s*(.*)$/mi', $raw_response['body'], $matches );
 			}
 
 			if ( empty( $matches[1] ) )
